c44ff0c61879f4bc0eff6a39f08d5e08686f1974
Essex97 <stratosxen@gmail.com>
2019-06-19 00:00:19 +0300

    "Final Commit"

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import numpy as np
+from copy import deepcopy
 from matplotlib import pyplot as plt
 
 
-def error(x_true, x_em):
+def cost_function(x_true, x_em):
     """
     :param x_true: The pixels of original image (normalized). Each row is an RGB vector.
     :param x_em: The image that comes from the training of em.
@@ -11,7 +12,7 @@ def error(x_true, x_em):
 
     n = x_true.shape[0]  # The The number of our examples (pixels)
 
-    return 1/n * np.sum((x_true - x_em)**2)
+    return 1/n * np.sum(np.linalg.norm(x_true - x_em)**2)
 
 
 def construct_image(height, width, g, m):
@@ -31,7 +32,7 @@ def construct_image(height, width, g, m):
         k = g[n].argmax()
         new_image[n] = m[k]
 
-    normalized = new_image / 255
+    flat = deepcopy(new_image)
 
     new_image = new_image.reshape((height, width, 3))
 
@@ -41,7 +42,7 @@ def construct_image(height, width, g, m):
     plt.savefig('em_{}'.format(k))
     plt.show()
 
-    return normalized, new_image
+    return flat, new_image
 
 
 def gaussian_mixture(x, p, m, s, k):
@@ -65,10 +66,6 @@ def gaussian_mixture(x, p, m, s, k):
         probabilities[:, k_i] = p[k_i] * np.prod(first_part * second_part, axis=1)
 
     return np.array(probabilities)
-
-
-# def log_likelihood(f, max_f):
-#     return np.sum(max_f + np.log(np.sum(np.exp(f - max_f.reshape((-1, 1))), axis=1)), axis=0)
 
 
 def log_likelihood(probabilities):
@@ -103,16 +100,6 @@ def maximization_step(x, g):
         p[k_i] = np.sum(g_k) / x.shape[0]
 
     return p, m, s
-
-
-# def expectation_step(f, max_f):
-#     """
-#     :return: The posterior probability of each category for all the pixel
-#     """
-#
-#     denominator = np.sum(np.exp(f - max_f.reshape((-1, 1))), axis=1)
-#
-#     return np.exp(f - max_f.reshape((-1, 1))) / denominator.reshape((-1, 1))
 
 
 def expectation_step(probabilities):
@@ -153,22 +140,6 @@ def initialize_parameters(k, d):
     return prior, m, s
 
 
-# def get_f(x, p, m, s, k):
-#
-#     f = np.zeros((x.shape[0], k))
-#
-#     for k_i in range(k):
-#         first_part = np.log(1 / np.sqrt(2 * np.pi * s[k_i]))
-#
-#         second_part = -0.5 * ((x - m[k_i]) ** 2) / s[k_i]
-#
-#         f[:, k_i] = np.log(p[k_i]) + first_part + np.sum(second_part, axis=1)
-#
-#     max_f = f.max(axis=1)
-#
-#     return f, max_f
-
-
 def expectation_maximization(x, k):
     """
     :param x: Table dimension (N x 3) with the pixels of th image
@@ -183,26 +154,18 @@ def expectation_maximization(x, k):
 
     prob = gaussian_mixture(x, prior, m, s, k)
 
-    # f, max_f = get_f(x, p, m, s, k)
+    for t in range(100):
 
-    for t in range(15):
-
-        # log_likelihood_old = log_likelihood(f, max_f)
         log_likelihood_old = log_likelihood(prob)
-
-        # g = expectation_step(f, max_f)
 
         g = expectation_step(prob)
 
         prior, m, s = maximization_step(x, g)
 
-        # f, max_f = get_f(x, p, m, s, k)
-
         prob = gaussian_mixture(x, prior, m, s, k)
 
-        # log_likelihood_new = log_likelihood(f, max_f)
         log_likelihood_new = log_likelihood(prob)
-        print('log_likelihood of {:<3} iteration: '.format(t, log_likelihood_new))
+        print('log_likelihood of {:<3} iteration: {}'.format(t, log_likelihood_new))
 
         if log_likelihood_new - log_likelihood_old < 0:
             print('Error in coding')
@@ -229,13 +192,13 @@ if __name__ == '__main__':
     # Normalize our data
     data = data/255
 
-    # categories = [1, 2, 4, 8, 16, 32, 64]
-    categories = 16
+    segments = [1, 2, 4, 8, 16, 32, 64]
 
-    post_probabilities, average_vectors = expectation_maximization(x=data, k=categories)
+    for seg in segments:
 
-    normal, new_img = construct_image(img.shape[0], img.shape[1], post_probabilities, average_vectors)
+        post_probabilities, average_vectors = expectation_maximization(x=data, k=seg)
+        flt, new_img = construct_image(img.shape[0], img.shape[1], post_probabilities, average_vectors)
 
-    error = error(data, normal)
+        error = cost_function(data, flt)
 
-    print(error)
+        print('Total error: {}'.format(error))
